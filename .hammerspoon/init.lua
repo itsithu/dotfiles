06bb3df573c15c6ed7ec0bf88474fa0c4153f717
Sithu Khant <sithuknt@gmail.com>
2025-09-21 15:45:05 +0630

    feat: add hammerspoon config

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,14 @@
+# brew install --cask hammerspoon
+# ~/.hammerspoon/init.lua
+
+local apps = {
+  ["2"] = "Safari",
+  ["3"] = "Zed",
+  ["4"] = "Terminal"
+}
+
+for key, app in pairs(apps) do
+  hs.hotkey.bind({"cmd"}, key, function()
+    hs.application.launchOrFocus(app)
+  end)
+end
